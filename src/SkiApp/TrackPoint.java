56bac6d69336a523fa86289adae2c99fa6718811
Rafał <rafal.wrzesniak94@gmail.com>
2020-04-22 00:32:23 +0200

    Added some testing input conditions

--- src/SkiApp/TrackPoint.java
+++ src/SkiApp/TrackPoint.java
@@ -4,7 +4,7 @@ import java.time.LocalTime;
 import java.time.LocalDateTime;
 
 
-class TrackPoint {
+public class TrackPoint {
 
     private double lat;
     private double lon;
@@ -12,15 +12,20 @@ class TrackPoint {
     private LocalDate date;
     private LocalTime time;
 
-    TrackPoint(String lat, String lon, String alt, String dateTime) {
-        this.lat = Double.parseDouble(lat);
-        this.lon = Double.parseDouble(lon);
-        this.alt = Double.parseDouble(alt);
+    public TrackPoint(String lat, String lon, String alt, String dateTime) {
+        setLat(lat);
+        setLon(lon);
+        setAlt(alt);
         setDate(trimInputZ(dateTime));
         setTime(trimInputZ(dateTime));
     }
 
     private String trimInputZ (String dateTime) {
+        if (dateTime == null) {
+            throw new IllegalArgumentException("DateTime argument cannot be null!");
+        } else if (dateTime.equals("")) {
+            throw new IllegalArgumentException("DateTime argument cannot be empty!");
+        }
         if(dateTime.endsWith("Z")){
             return dateTime.substring(0, dateTime.length()-1);
         } else {
@@ -36,29 +41,56 @@ class TrackPoint {
         this.time = LocalDateTime.parse(dateTime).toLocalTime();
     }
 
+    private void setLat(String lat) {
+        if (lat == null) {
+            throw new IllegalArgumentException("Latitude argument cannot be null!");
+        } else if (lat.equals("")) {
+            throw new IllegalArgumentException("Latitude argument cannot be empty!");
+        }
+        this.lat = Double.parseDouble(lat);
+    }
+
+    private void setLon(String lon) {
+        if (lon == null) {
+            throw new IllegalArgumentException("Longitude argument cannot be null!");
+        } else if (lon.equals("")) {
+            throw new IllegalArgumentException("Longitude argument cannot be empty!");
+        }
+        this.lon = Double.parseDouble(lon);
+    }
+
+    private void setAlt(String alt) {
+        if (alt == null) {
+            throw new IllegalArgumentException("Altitude argument cannot be null!");
+        } else if (alt.equals("")) {
+            throw new IllegalArgumentException("Altitude argument cannot be empty!");
+        }
+        this.alt = Double.parseDouble(alt);
+    }
+
     void printTrackPoint() {
         System.out.println(String.join(": ", "DateTime", String.join(" ", getDate().toString(), getTime().toString())));
         System.out.println(String.join(": ", "Position", String.join(", ", String.valueOf(getLat()), String.valueOf(getLon()))));
         System.out.println(String.join(": ", "Altitude", String.valueOf(getAlt())));
     }
 
-    double getLat() {
+    public double getLat() {
         return lat;
     }
 
-    double getLon() {
+    public double getLon() {
         return lon;
     }
 
-    double getAlt() {
+    public double getAlt() {
         return alt;
     }
 
-    LocalDate getDate() {
+    public LocalDate getDate() {
         return date;
     }
 
-    LocalTime getTime() {
+    public LocalTime getTime() {
         return time;
     }
 }
